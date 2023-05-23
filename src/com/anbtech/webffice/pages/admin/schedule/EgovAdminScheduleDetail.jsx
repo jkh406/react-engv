07690198515932062cbb31cf5670c22009c50fb1
DESKTOP-FPLD98H\anb <anb>
2023-05-23 14:22:53 +0900

    22

--- src/com/anbtech/webffice/pages/admin/schedule/EgovAdminScheduleDetail.jsx
+++ src/com/anbtech/webffice/pages/admin/schedule/EgovAdminScheduleDetail.jsx
@@ -10,7 +10,7 @@ import EgovAttachFile from '@components/EgovAttachFile';
 
 function EgovAdminScheduleDetail(props) {
     console.group("EgovAdminScheduleDetail");
-    console.log("[Start] EgovAdminScheduleD123123123123213etail ------------------------------");
+    console.log("[Start] EgovAdminScheduleD12312312223123213etail ------------------------------");
     console.log("EgovAdminScheduleDetail [props] : ", props);
 
     const navigate = useNavigate();
