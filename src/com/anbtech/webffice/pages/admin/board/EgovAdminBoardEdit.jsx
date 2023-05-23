890a5377215d39842a6f4233769fa0891fbb6408
DESKTOP-FPLD98H\anb <anb>
2023-05-23 14:31:02 +0900

    333

--- src/com/anbtech/webffice/pages/admin/board/EgovAdminBoardEdit.jsx
+++ src/com/anbtech/webffice/pages/admin/board/EgovAdminBoardEdit.jsx
@@ -10,7 +10,7 @@ import EgovRadioButtonGroup from '@components/EgovRadioButtonGroup';
 
 function EgovAdminBoardEdit(props) {
     console.group("EgovAdminBoardEdit");
-    console.log("[Start] EgovAdmi123123nBoardEdit ------------------------------");
+    console.log("[Start] EgovAdmi123111123nBoardEdit ------------------------------");
     console.log("EgovAdminBoardEdit [props] : ", props);
 
     const navigate = useNavigate();
